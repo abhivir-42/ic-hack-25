9d4b5fa26ec4fb438732a8d25b3ddeafeb6b61b3
Daiki Kawagishi <daikikawagishi@gmail.com>
2025-02-01 15:51:29 +0000

    deno deck.gl setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import { Map } from 'react-map-gl/maplibre';
-import DeckGL from '@deck.gl/react';
-import { MapViewState } from '@deck.gl/core';
+import MapComponent from "@/components/Map";
 
-const INITIAL_VIEW_STATE: MapViewState = {
-  longitude: -1.415727,
-  latitude: 52.232395,
-  zoom: 6.6,
-  minZoom: 5,
-  maxZoom: 15,
-  pitch: 40.5,
-  bearing: -27
-};
-
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
-
-export default function MapComponent({ mapStyle = MAP_STYLE }: { mapStyle?: string }) {
+export default function Home() {
   return (
-    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
-      <Map reuseMaps mapStyle={mapStyle} />
-    </DeckGL>
+    <main className="w-screen h-screen">
+      <MapComponent />
+    </main>
   );
 }
